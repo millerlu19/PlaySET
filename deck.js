d66c033990eb180f167884df7420b132a1563881
Lucas Miller <millerlu19@hanover.edu>
2019-03-22 14:14:35 -0400

    created deck class and started implementation

---
+++ deck.js
@@ -0,0 +1,47 @@
+// deck.js
+
+import Card from "card.js";
+
+let colorsArr = Object.values(Card.colors)
+let shapesArr = Object.values(Card.shapes)
+let patternArr = Object.values(Card.pattern)
+let numberArr = Object.values(Card.number)
+
+export default class Deck {
+	constructor() {
+		this.cardList = [];
+	}
+
+	addToDeck(card) {
+		this.cardList.push(card);
+	}
+
+	take(num) {
+
+	}
+
+	removeCard() {
+
+	}
+
+	removeSET() {
+
+	}
+
+}
+
+function makeFullDeck() {
+	let fullDeck = new Deck();
+	for (let c of colorsArr) {
+		for (let s of shapesArr) {
+			for (let p of patternArr) {
+				for (let n of numberArr) {
+					let card = new Card(c, s, p, n);
+					fullDeck.addToDeck(card);
+				}
+			}
+		}
+	}
+
+	
+}
---
+++ js/deck.js
@@ -0,0 +1,30 @@
+// deck.js
+
+import card.js;
+
+export default class Deck {
+	constructor() {
+		this.cardList = [];
+	}
+
+	addToDeck(card) {
+
+	}
+
+	take(num) {
+
+	}
+
+	removeCard() {
+
+	}
+
+	removeSET() {
+
+	}
+
+}
+
+function makeFullDeck() {
+	new Deck();
+}
