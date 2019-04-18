6e76be27640020f8bf89065b090ee53ee4dcaebe
Jonathan Helmers <helmersj20@hanover.edu>
2019-04-18 01:09:16 -0400

    Added clickable event and example cards

--- js/PlaySet.js
+++ js/PlaySet.js
@@ -6,14 +6,38 @@ import Selector from "./selector.js";
 
 export default class PlaySet {
 
-console.log("running");
-var draw = SVG('drawing').size(300, 300);
+
+}
+
+var draw = SVG('drawing').size(500, 500);
 var rect = draw.rect(100, 150).fill('#000000').move(10,10);
+var text = draw.plain('red').fill('#FFFFFF').move(10,10);
+var text = draw.plain('oval').fill('#FFFFFF').move(10,20);
+var text = draw.plain('empty').fill('#FFFFFF').move(10,30);
+var text = draw.plain('one').fill('#FFFFFF').move(10,40);
+var rect2 = draw.rect(100, 150).fill('#000000').move(120, 10);
+var text = draw.plain('red').fill('#FFFFFF').move(120,10);
+var text = draw.plain('oval').fill('#FFFFFF').move(120,20);
+var text = draw.plain('empty').fill('#FFFFFF').move(120,30);
+var text = draw.plain('two').fill('#FFFFFF').move(120,40);
+var rect3 = draw.rect(100, 150).fill('#000000').move(230, 10);
+var text = draw.plain('red').fill('#FFFFFF').move(230,10);
+var text = draw.plain('oval').fill('#FFFFFF').move(230,20);
+var text = draw.plain('empty').fill('#FFFFFF').move(230,30);
+var text = draw.plain('three').fill('#FFFFFF').move(230,40);
+var i = 0;
 var click = function() {
-	this.fill({color:'#32CD32'});
-	console.log("click");
+	if (i%2 == 0){
+		this.fill({color:'#32CD32'});
+		i++;
+	}
+	else{
+		this.fill({color: '#000000'});
+		i++;
+	}
+	
 }
 
 rect.on('click', click);
-}
-
+rect2.on('click', click);
+rect3.on('click', click);
